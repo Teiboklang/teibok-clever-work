cc4011110bfae7a28922e8300258e528dd2946df
Teiboklang <100836994+Teiboklang@users.noreply.github.com>
2022-03-21 20:34:29 +0530

    Add files via upload

---
+++ application.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title> application form</title>
+	<link rel="stylesheet" href="D:\web/form.css"/>
+	</head>
+		<body>
+
+	<div class="form">
+	<h1>Read the instruction before filling the form</h1>
+	</div>
+	<div class="inst">
+	
+			<p>1. This application form is to be filled by students seeking admission into XYZ Higher Secondary Schools in Meghalaya.<br> 2.Mere submission of this form will not guarantee admission into the school.<br>3. Merit list would be generated after scrutiny.<br>
+			4. Students can fill the same form multiple times choosing different schools or different streams<br>
+			5. Kindly enter proper email ID for any corresponce with the school<br>
+			6. Please check the Course Stream and the Subject Combinations available in a particular higher secondary school before proceeding to fill up the form<br><a href="D:\web/course.html">click here</a><br>
+			8. For any technical issues: maheh2260@gmail.com/ whatapp 8974003445.
+			</p>
+	</div>
+	<hr color="blue" width="60%"/>
+	<div class="back">
+    <a href="index.html"><img src="D:\web/back1.gif"></a>
+	</div>
+	
+	<div class="app">
+	
+	<a href="D:\web/form.html"><img src="D:\web/apply-now.gif"></a>
+	</div>
+	<hr color="red" size="6" width="50%"/>
+	</body>
+</html>
